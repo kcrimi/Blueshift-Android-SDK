36f4c477aad46ecab52261f4c5c51f02b1098ec6
Rahul Raveendran V P <rahul.pmna@gmail.com>
2020-02-14 12:29:15 +0530

    reformatted the code for reuse

--- android-sdk/src/main/java/com/blueshift/BlueshiftNotification.java
+++ android-sdk/src/main/java/com/blueshift/BlueshiftNotification.java
@@ -2,19 +2,27 @@ package com.blueshift;
 
 import android.app.Notification;
 import android.app.NotificationChannel;
+import android.app.PendingIntent;
 import android.content.Context;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
 import android.os.Build;
+import android.text.TextUtils;
 
+import androidx.core.app.NotificationCompat;
 import androidx.core.app.NotificationManagerCompat;
 
+import com.blueshift.model.Configuration;
 import com.blueshift.rich_push.Message;
+import com.blueshift.util.BlueshiftUtils;
 import com.blueshift.util.NotificationUtils;
 
 abstract class BlueshiftNotification {
     private static final String TAG = "BlueshiftNotification";
 
-    Context mContext;
-    Message mMessage;
+    private Context mContext;
+    private Message mMessage;
+    private NotificationCompat.Builder mBuilder;
 
     BlueshiftNotification(Context context, Message message) {
         this.mContext = context;
@@ -35,6 +43,37 @@ abstract class BlueshiftNotification {
         return status;
     }
 
+    public Context getContext() {
+        return mContext;
+    }
+
+    public Message getMessage() {
+        return mMessage;
+    }
+
+    private void initNotificationBuilder() {
+        try {
+            String channelId = NotificationUtils.getNotificationChannelId(mContext, mMessage);
+
+            mBuilder = new NotificationCompat.Builder(mContext, channelId);
+
+            addBasicConfig(mBuilder);
+            addMandatoryContent(mBuilder);
+            addBasicContent(mBuilder);
+            addActions(mBuilder);
+        } catch (Exception e) {
+            BlueshiftLogger.e(TAG, e);
+        }
+    }
+
+    NotificationCompat.Builder getNotificationBuilder() {
+        if (mBuilder == null) {
+            initNotificationBuilder();
+        }
+
+        return mBuilder;
+    }
+
     private void createChannel(NotificationManagerCompat manager) {
         try {
             if (manager != null) {
@@ -45,6 +84,90 @@ abstract class BlueshiftNotification {
                         manager.createNotificationChannel(channel);
                     }
                 }
+            }
+        } catch (Exception e) {
+            BlueshiftLogger.e(TAG, e);
+        }
+    }
+
+    private void addBasicConfig(NotificationCompat.Builder builder) {
+        if (builder != null) {
+            builder.setDefaults(Notification.DEFAULT_SOUND);
+            builder.setAutoCancel(true);
+            builder.setPriority(NotificationCompat.PRIORITY_MAX);
+        }
+    }
+
+    private void addMandatoryContent(NotificationCompat.Builder builder) {
+        try {
+            Configuration config = BlueshiftUtils.getConfiguration(mContext);
+            if (config != null) {
+                int smallIconResId = config.getSmallIconResId();
+                if (smallIconResId != 0) builder.setSmallIcon(smallIconResId);
+
+                int color = config.getNotificationColor();
+                if (color != 0) builder.setColor(color);
+
+                int bigIconResId = config.getLargeIconResId();
+                if (bigIconResId != 0) {
+                    Bitmap bigIcon = BitmapFactory.decodeResource(mContext.getResources(), bigIconResId);
+                    if (bigIcon != null) builder.setLargeIcon(bigIcon);
+                }
+            }
+        } catch (Exception e) {
+            BlueshiftLogger.e(TAG, e);
+        }
+    }
+
+    private void addBasicContent(NotificationCompat.Builder builder) {
+        try {
+            builder.setContentTitle(mMessage.getContentTitle());
+            builder.setContentText(mMessage.getContentText());
+
+            if (!TextUtils.isEmpty(mMessage.getContentSubText())) {
+                builder.setSubText(mMessage.getContentSubText());
+            }
+        } catch (Exception e) {
+            BlueshiftLogger.e(TAG, e);
+        }
+    }
+
+    private void addActions(NotificationCompat.Builder builder) {
+        try {
+            // pending intent that opens the app using MAIN activity.
+            PendingIntent openAppPendingIntent = NotificationUtils.getOpenAppPendingIntent(mContext, mMessage, getNotificationId());
+
+            switch (mMessage.getCategory()) {
+                case Buy:
+                    PendingIntent viewPendingIntent = NotificationUtils.getViewActionPendingIntent(mContext, mMessage, getNotificationId());
+                    builder.addAction(0, "View", viewPendingIntent);
+
+                    PendingIntent buyPendingIntent = NotificationUtils.getBuyActionPendingIntent(mContext, mMessage, getNotificationId());
+                    builder.addAction(0, "Buy", buyPendingIntent);
+
+                    builder.setContentIntent(openAppPendingIntent);
+
+                    break;
+
+                case ViewCart:
+                    PendingIntent openCartPendingIntent = NotificationUtils.getOpenCartPendingIntent(mContext, mMessage, getNotificationId());
+                    builder.addAction(0, "Open Cart", openCartPendingIntent);
+
+                    builder.setContentIntent(openAppPendingIntent);
+
+                    break;
+
+                case Promotion:
+                    PendingIntent openPromoPendingIntent = NotificationUtils.getOpenPromotionPendingIntent(mContext, mMessage, getNotificationId());
+                    builder.setContentIntent(openPromoPendingIntent);
+
+                    break;
+
+                default:
+                    /*
+                     * Default action is to open app and send all details as extra inside intent
+                     */
+                    builder.setContentIntent(openAppPendingIntent);
             }
         } catch (Exception e) {
             BlueshiftLogger.e(TAG, e);
--- android-sdk/src/main/java/com/blueshift/BlueshiftNotificationNative.java
+++ android-sdk/src/main/java/com/blueshift/BlueshiftNotificationNative.java
@@ -1,7 +1,6 @@
 package com.blueshift;
 
 import android.app.Notification;
-import android.app.PendingIntent;
 import android.content.Context;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
@@ -39,43 +38,17 @@ class BlueshiftNotificationNative extends BlueshiftNotification {
     Notification getNotification() {
         try {
             // build the notification here
-            if (mMessage != null) {
-                Configuration config = BlueshiftUtils.getConfiguration(mContext);
+            if (getMessage() != null) {
+                Configuration config = BlueshiftUtils.getConfiguration(getContext());
                 if (config != null) {
-                    String channelId = NotificationUtils.getNotificationChannelId(mContext, mMessage);
-
-                    NotificationCompat.Builder builder = new NotificationCompat.Builder(mContext, channelId);
-
-                    // BASIC -> TODO: Check with @aswani
-                    builder.setDefaults(Notification.DEFAULT_SOUND);
-                    builder.setAutoCancel(true);
-                    builder.setPriority(NotificationCompat.PRIORITY_MAX);
-
-                    // MANDATORY
-                    int smallIconResId = config.getSmallIconResId();
-                    if (smallIconResId != 0) builder.setSmallIcon(smallIconResId);
-
-                    int color = config.getNotificationColor();
-                    if (color != 0) builder.setColor(color);
-
-                    int bigIconResId = config.getLargeIconResId();
-                    if (bigIconResId != 0) {
-                        Bitmap bigIcon = BitmapFactory.decodeResource(mContext.getResources(), bigIconResId);
-                        if (bigIcon != null) builder.setLargeIcon(bigIcon);
-                    }
-
-                    // CONTENT
-                    addBasicContent(builder);
+                    NotificationCompat.Builder builder = getNotificationBuilder();
 
                     // STYLE
-                    if (mMessage.hasImage()) {
-                        enableBigPictureStyle(builder);
+                    if (getMessage().hasImage()) {
+                        addBigPictureStyle(builder);
                     } else {
-                        enableBigTextStyle(builder);
+                        addBigTextStyle(builder);
                     }
-
-                    // ACTION
-                    addActions(builder);
 
                     return builder.build();
                 } else {
@@ -89,33 +62,20 @@ class BlueshiftNotificationNative extends BlueshiftNotification {
         return null;
     }
 
-    private void addBasicContent(NotificationCompat.Builder builder) {
+    private void addBigPictureStyle(NotificationCompat.Builder builder) {
         try {
-            builder.setContentTitle(mMessage.getContentTitle());
-            builder.setContentText(mMessage.getContentText());
-
-            if (!TextUtils.isEmpty(mMessage.getContentSubText())) {
-                builder.setSubText(mMessage.getContentSubText());
-            }
-        } catch (Exception e) {
-            BlueshiftLogger.e(TAG, e);
-        }
-    }
-
-    private void enableBigPictureStyle(NotificationCompat.Builder builder) {
-        try {
-            URL imageURL = new URL(mMessage.getImageUrl());
+            URL imageURL = new URL(getMessage().getImageUrl());
             Bitmap bitmap = BitmapFactory.decodeStream(imageURL.openStream());
             if (bitmap != null) {
                 NotificationCompat.BigPictureStyle bigPictureStyle = new NotificationCompat.BigPictureStyle();
                 bigPictureStyle.bigPicture(bitmap);
 
-                if (!TextUtils.isEmpty(mMessage.getBigContentTitle())) {
-                    bigPictureStyle.setBigContentTitle(mMessage.getBigContentTitle());
+                if (!TextUtils.isEmpty(getMessage().getBigContentTitle())) {
+                    bigPictureStyle.setBigContentTitle(getMessage().getBigContentTitle());
                 }
 
-                if (!TextUtils.isEmpty(mMessage.getBigContentSummaryText())) {
-                    bigPictureStyle.setSummaryText(mMessage.getBigContentSummaryText());
+                if (!TextUtils.isEmpty(getMessage().getBigContentSummaryText())) {
+                    bigPictureStyle.setSummaryText(getMessage().getBigContentSummaryText());
                 }
 
                 builder.setStyle(bigPictureStyle);
@@ -125,71 +85,29 @@ class BlueshiftNotificationNative extends BlueshiftNotification {
         }
     }
 
-    private void enableBigTextStyle(NotificationCompat.Builder builder) {
+    private void addBigTextStyle(NotificationCompat.Builder builder) {
         try {
             // enable big text style
             boolean enableBigStyle = false;
 
             NotificationCompat.BigTextStyle bigTextStyle = new NotificationCompat.BigTextStyle();
 
-            if (!TextUtils.isEmpty(mMessage.getBigContentTitle())) {
+            if (!TextUtils.isEmpty(getMessage().getBigContentTitle())) {
                 enableBigStyle = true;
-                bigTextStyle.setBigContentTitle(mMessage.getBigContentTitle());
+                bigTextStyle.setBigContentTitle(getMessage().getBigContentTitle());
             }
 
-            if (!TextUtils.isEmpty(mMessage.getBigContentSummaryText())) {
+            if (!TextUtils.isEmpty(getMessage().getBigContentSummaryText())) {
                 enableBigStyle = true;
-                bigTextStyle.setSummaryText(mMessage.getBigContentSummaryText());
+                bigTextStyle.setSummaryText(getMessage().getBigContentSummaryText());
             }
 
-            if (!TextUtils.isEmpty(mMessage.getContentText())) {
-                bigTextStyle.bigText(mMessage.getContentText());
+            if (!TextUtils.isEmpty(getMessage().getContentText())) {
+                bigTextStyle.bigText(getMessage().getContentText());
             }
 
             if (enableBigStyle) {
                 builder.setStyle(bigTextStyle);
-            }
-        } catch (Exception e) {
-            BlueshiftLogger.e(TAG, e);
-        }
-    }
-
-    private void addActions(NotificationCompat.Builder builder) {
-        try {
-            // pending intent that opens the app using MAIN activity.
-            PendingIntent openAppPendingIntent = NotificationUtils.getOpenAppPendingIntent(mContext, mMessage, getNotificationId());
-
-            switch (mMessage.getCategory()) {
-                case Buy:
-                    PendingIntent viewPendingIntent = NotificationUtils.getViewActionPendingIntent(mContext, mMessage, getNotificationId());
-                    builder.addAction(0, "View", viewPendingIntent);
-
-                    PendingIntent buyPendingIntent = NotificationUtils.getBuyActionPendingIntent(mContext, mMessage, getNotificationId());
-                    builder.addAction(0, "Buy", buyPendingIntent);
-
-                    builder.setContentIntent(openAppPendingIntent);
-
-                    break;
-
-                case ViewCart:
-                    PendingIntent openCartPendingIntent = NotificationUtils.getOpenCartPendingIntent(mContext, mMessage, getNotificationId());
-                    builder.addAction(0, "Open Cart", openCartPendingIntent);
-
-                    builder.setContentIntent(openAppPendingIntent);
-
-                    break;
-
-                case Promotion:
-                    PendingIntent openPromoPendingIntent = NotificationUtils.getOpenPromotionPendingIntent(mContext, mMessage, getNotificationId());
-                    builder.setContentIntent(openPromoPendingIntent);
-
-                    break;
-
-                default:
-                    /*
-                     * Default action is to open app and send all details as extra inside intent
-                     */
-                    builder.setContentIntent(openAppPendingIntent);
             }
         } catch (Exception e) {
             BlueshiftLogger.e(TAG, e);
